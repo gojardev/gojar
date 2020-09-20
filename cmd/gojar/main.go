0fd3807dbe30a3bb01e2d2bbfbc7df3749554b20
Marwan Sulaiman <marwan.sameer@gmail.com>
2020-09-20 16:52:17 -0400

    add cli

--- cmd/gojar/main.go
+++ cmd/gojar/main.go
@@ -1,5 +1,111 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/manifoldco/promptui"
+	"github.com/urfave/cli/v2"
+	gojar "src.gojar.dev"
+)
+
+func main() {
+	app := &cli.App{
+		Name: "gojar",
+		Commands: []*cli.Command{
+			{
+				Name: "login",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "registry",
+						Aliases: []string{"r"},
+						Usage:   "The registry URL to verify your login credentials and retrieve your GOPROXY URL",
+						// Value:   "", // TODO: gojar service url
+					},
+					&cli.BoolFlag{
+						Name:  "token-stdin",
+						Usage: "Retrieve the token credential from stdin",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					if err := login(c); err != nil {
+						return cli.Exit(err.Error(), 1)
+					}
+					return nil
+				},
+			},
+			{
+				Name: "logout",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name: "registry", // to remove from .netrc
+					},
+				},
+				Action: func(c *cli.Context) error {
+					cmd := exec.CommandContext(c.Context, "go", "env", "-w", "GOPROXY=proxy.golang.org,direct")
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+					err := cmd.Run()
+					if err != nil {
+						return fmt.Errorf("could not rewrite GOPROXY: %w", err)
+					}
+					// TODO: remove from .netrc
+					return nil
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func login(c *cli.Context) error {
+	registry := c.String("registry")
+	var token string
+	var err error
+	if c.Bool("token-stdin") {
+		_, err = fmt.Scanln(&token)
+		if err != nil {
+			return fmt.Errorf("could not scan token from stdin: %w", err)
+		}
+	} else {
+		prompt := promptui.Prompt{
+			Label: "Token",
+			Validate: func(s string) error {
+				if strings.Contains(s, " ") {
+					return fmt.Errorf("must not contain spaces")
+				}
+				return nil
+			},
+		}
+		token, err = prompt.Run()
+		if err != nil {
+			return fmt.Errorf("could not get token from prompt: %w", err)
+		}
+	}
+	gojarClient := gojar.NewAPIProtobufClient(registry, http.DefaultClient)
+	resp, err := gojarClient.ProxyURL(c.Context, &gojar.ProxyURLRequest{ProxyToken: token})
+	if err != nil {
+		// TODO: nicer message when invalid auth
+		return fmt.Errorf("error authenticating: %w", err)
+	}
+	proxyURL := resp.GetURL()
+	url, err := url.Parse(proxyURL)
+	if err != nil {
+		return fmt.Errorf("error parsing %q: %w", proxyURL, err)
+	}
+	writeNETRC(url.Hostname(), "gojar", token, "")
+	// TODO: integrate with existing GOPROXY instead of overriding everything
+	cmd := exec.CommandContext(c.Context, "go", "env", "-w", fmt.Sprintf("GOPROXY=%s", proxyURL))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("could not persist GOPROXY: %w", err)
+	}
+	return nil
 }
